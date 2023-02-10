5e7ed27b87fec5d12fc005fe08e3e626b0608822
sercan-esen <sercan1esen@gmail.com>
2023-02-10 14:27:09 +0300

    SongCatalog added

---
+++ app/src/main/java/com/example/practicekotlinfundamentals/SongCatalog.kt
@@ -0,0 +1,21 @@
+package com.example.practicekotlinfundamentals
+
+fun main() {
+    val anotherLoveSong = Song("Another Love","Tom Odell",2013,638_000_000)
+    anotherLoveSong.printDesc()
+    println(anotherLoveSong.isPopular)
+}
+
+class Song(
+    val title: String,
+    val artist: String,
+    val yearPublished: Int,
+    val playCount: Int
+) {
+    val isPopular: Boolean
+        get() = playCount >= 1000
+
+    fun printDesc() {
+        println("$title,performed by $artist,was released in $yearPublished. ")
+    }
+}
