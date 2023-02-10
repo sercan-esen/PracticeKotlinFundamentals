b68efb63489fcb04414267b63cf8ede1d0aa7372
sercan-esen <sercan1esen@gmail.com>
2023-02-10 14:47:38 +0300

    InternetProfile formatted

--- app/src/main/java/com/example/practicekotlinfundamentals/InternetProfile.kt
+++ app/src/main/java/com/example/practicekotlinfundamentals/InternetProfile.kt
@@ -3,7 +3,7 @@ package com.example.practicekotlinfundamentals
 fun main() {
     val amanda = Person("Amanda", 33, "play tennis", null)
     val atiqah = Person("Atiqah", 28, "climb", amanda)
-    val sercan = Person("Sercan",26,"play football",atiqah)
+    val sercan = Person("Sercan", 26, "play football", atiqah)
 
     amanda.showProfile()
     atiqah.showProfile()
